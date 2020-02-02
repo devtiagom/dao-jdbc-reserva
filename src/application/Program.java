785d2532cdf4df66e2aa878da3a0c67b83488dfa
Tiago Machado <tiago.o.machado@gmail.com>
2020-02-02 20:40:35 -0300

    TipoPasseio entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
 import model.entities.Modalidade;
+import model.entities.TipoPasseio;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Modalidade m = new Modalidade(1, "Caminhada");
+		TipoPasseio tp = new TipoPasseio(1, "Em Grupo", "Passeio para grupos abertos");
+		
 		System.out.println(m);
+		System.out.println(tp);
 	}
 
 }
